b0c706ff7fdf19b58a32b32ced908c0ff534a2af
Cauã Felipe <devcaua098@gmail.com>
2026-08-14 16:08:31 -0300

    Enhance salary extraction with data transformation
    Added data transformation and cleaning steps, including pivoting and renaming columns, to the salary extraction process.

--- Extracao_salario_medio_SIDRAR.R
+++ Extracao_salario_medio_SIDRAR.R
@@ -1,6 +1,7 @@
 library(sidrar)
 library(janitor)
 library(dplyr)
+library(tidyr)
 
 # Montando Requisição  ----------------------------------------------------
 
@@ -37,7 +38,27 @@ dados_limpos <- dados_limpos |>
     valor = as.numeric(valor),       #Muda o tipo de variável de caracter para numérico
     orgao_fonte = "IBGE/SIDRA"       #Adiciona uma coluna orgao_fonte com a descrição
   ) |> 
-  filter(!is.na(valor))              #Remove tudo que não possui um valor
+  filter(!is.na(valor)) |>           #Remove tudo que não possui um valor
+  pivot_wider(
+      names_from = unidade_de_medida,     # Pega os nomes das observações presentes naquela coluna
+      values_from = valor                 # Valores correspondentes a cada observação e que preencherão novas colunas 
+  ) |> 
+  clean_names() |> 
+  rename(salario_minimo = salarios_minimos) |> 
+  relocate(salario_minimo, .after = municipio_codigo) |> 
+  relocate(c(reais, unidades), .after = salario_minimo)
+
+# Separa os tipos de dados por coluna e aplica redução de valor NA
+resultado <- dados_limpos |> 
+  group_by(municipio_codigo, municipio, ano) |>         #Agrupa por código de municipio, nome e ano
+  
+  #Resume cada grupo em uma única linha, extraindo o primeiro valor não-nulo de cada coluna
+  summarise(
+    salario_minimo = first(na.omit(salario_minimo)),
+    salario_reais = first(na.omit(reais)),
+    unidades = first(na.omit(unidades)),
+    .groups = "drop"
+  )
 
 #exporta o resultado em csv
-write.csv(dados_limpos, "salario_medio_mensal.csv", row.names = FALSE, fileEncoding = "UTF-8")
+write.csv(resultado, "salario_medio_mensal.csv", row.names = FALSE, fileEncoding = "UTF-8")
